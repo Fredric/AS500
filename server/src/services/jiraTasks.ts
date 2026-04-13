4bfb849a0a6b6d64275d72dacba0e798948b14c2
Fredric Berling <fb@stepwise.no>
2026-04-13 20:37:25 +0200

    jira

---
+++ server/src/services/jiraTasks.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+export interface jiraTask {
+    id: string;
+    name: string;
+    description: string;
+    status: string;
+    assignee: string;
+  }
+
+const JIRA_BASE_URL = process.env.JIRA_BASE_URL;
+const EMAIL = "fb@stepwise.no";
+const API_TOKEN = process.env.JIRA_API_TOKEN ?? '';
+
+const jira = axios.create({
+    baseURL: 'https://stepwise-as.atlassian.net',
+    auth: {
+      username: EMAIL,
+      password: API_TOKEN
+
+    },
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  
+  export async function getJiraTasks(): Promise<jiraTask[]> {
+    const jql = "assignee = currentUser() AND (status != Done OR resolution != Done) ORDER BY created DESC"
+    const response = await jira.get('/rest/api/3/search/jql', {
+      params: {
+        jql,
+        maxResults: 25,
+        includeCollapsedFields: true,
+        fields: "key,summary,status,assignee"
+      }
+    });
+    
+    const data = response.data.issues.map((issue: any) => ({
+      id: issue.key,
+      name: issue.fields.summary,
+      status: issue.fields.status.name,
+      assignee: issue.fields.assignee.displayName
+    }));
+    
+    return data;
+  }
